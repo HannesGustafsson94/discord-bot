19ce372fb7f06d1c49fbccc6f10923eaa10f83cf
HannesGustafsson94 <63160800+HannesGustafsson94@users.noreply.github.com>
2020-12-23 16:28:53 +0100

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -4,24 +4,24 @@ module.exports = {
     description: 'Send a random gif',
     callback: async (message) => {
         message.delete();
-        const fetch = require('node-fetch')
-        let keywords = 'excited'
-        let tokens = message.content.split(' ')
+        try {
+            const fetch = require('node-fetch')
+            let keywords = 'excited'
+            let tokens = message.content.split(' ')
 
-        if (tokens.length > 1) {
-            keywords = tokens.slice(1, tokens.length).join("")
-        }
-        let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}&contentfilter=off`
-        let response = await fetch(url);
-        let json = await response.json()
-        const index = Math.floor(Math.random() * json.results.length)
-        if (json.results[index].url) {
-            try {
+            if (tokens.length > 1) {
+                keywords = tokens.slice(1, tokens.length).join("")
+            }
+            let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}&contentfilter=off`
+            let response = await fetch(url);
+            let json = await response.json()
+            const index = Math.floor(Math.random() * json.results.length)
+            if (json.results[index].url) {
                 message.channel.send(json.results[index].url)
             }
-            catch(err) {
-                message.channel.send('Search invalid')
-            }
+        }
+        catch {
+            message.channel.send('Invalid search term')
         }
     }
 }
